193bc9d3a268f5696758ab3e27b6431b047e2f99
omarakamal <54825038+omarakamal@users.noreply.github.com>
2023-08-12 03:40:40 +0400

    Revert "new route"

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -9,4 +9,20 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.post('/new-user',fileUploader.array('pictureOfDog'),(req,res)=>{
+  console.log(req.files)
+  res.send(req.files[0].path)
+  // const {email,password} = req.body
+  // User.create({email,password})
+  // .then(()=>{
+  //   res.redirect('/')
+  // })
+  // .catch((err)=>{
+  //   if(err.http_code===400){
+  //     res.render('index',{errorMessage:"format not allowed please upload jpg or png"})
+  //   }
+  //   console.log(err)
+  // })
+})
+
 module.exports = router;
